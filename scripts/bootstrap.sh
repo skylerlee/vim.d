98f3974184cf39fd656bd2fe025fe23b5b298d97
skylerlee <skyler.ac.lee@gmail.com>
2019-01-13 00:06:11 +0800

    feat: add dependency

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,19 +4,20 @@ DOTVIMD=$HOME/.vim.d
 
 DEPENDENCIES=(
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/skylerlee/vim-infra/master/autoload/infra.vim
 )
 
-function fetch_plugin {
+function fetch_file {
   local file_url="$1"
   local dest_dir="$2"
   local file_name=${file_url##*/}
+  mkdir -p $dest_dir
   curl -fsSL "$file_url" > $dest_dir/$file_name
 }
 
 function prefetch_plugins {
-  mkdir -p $DOTVIMD/autoload
   for dep in $DEPENDENCIES; do
-    fetch_plugin $dep $DOTVIMD/autoload
+    fetch_file $dep $DOTVIMD/autoload
   done
 }
 
